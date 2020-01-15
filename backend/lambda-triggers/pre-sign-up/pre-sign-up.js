b2cf89373e8884d39574d27258f3d49517fd976c
Dennis Hills <dmennis@gmail.com>
2020-01-14 17:04:55 -0800

    updated the pre-sign-up function to log event

--- backend/lambda-triggers/pre-sign-up/pre-sign-up.js
+++ backend/lambda-triggers/pre-sign-up/pre-sign-up.js
@@ -1,6 +1,7 @@
 "use strict";
 
 exports.handler = async (event) => {
+    console.log('Received EVENT', JSON.stringify(event, null, 2));
     event.response.autoConfirmUser = true;
     event.response.autoVerifyPhone = true;
     return event;
